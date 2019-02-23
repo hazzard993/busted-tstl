0ab3cc7057b2fd7ccd81ec34f1e62787d06b6808
hazzard993 <hazzard993@gmail.com>
2019-02-23 19:50:45 +1000

    Removed noLib and added header

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,7 @@
-/// <reference no-default-lib="true" />
+// Typescript definitions for LÖVE 2D
+// Project: https://github.com/hazzard993/love-typescript-definitions
+// Definitions by: Jason McKenzie <https://github.com/hazzard993>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.6
 
 /// <reference path="busted.d.ts" />
